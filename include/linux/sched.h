cdb99249bb20423a0fc09fe17c5062b5efe74e76
Pradeep P V K <pragalla@codeaurora.org>
2021-05-10 15:30:56 +0530

    Revert "fs: fuse: Implement fuse short circuit"
    This reverts commit efee1432663b4641ed37b8c6fbc70323ad1b7cf2.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1393,10 +1393,6 @@ struct task_struct {
 	void				*security;
 #endif
 
-#ifdef CONFIG_FUSE_SHORTCIRCUIT
-	int fuse_boost;
-#endif
-
 	struct {
 		struct work_struct work;
 		atomic_t running;
@@ -1409,8 +1405,6 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
-	struct fuse_package *fpack;
-
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1420,12 +1414,6 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
-};
-
-struct fuse_package {
-	bool fuse_open_req;
-	struct file *filp;
-	char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
