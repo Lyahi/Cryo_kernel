37517c52c9418e9ff98882a0a36a3c5be64e5645
lyahi <kieras48@gmail.com>
2024-02-13 19:54:28 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -3,30 +3,25 @@
 SECONDS=0 # builtin bash timer
 head=$(git rev-parse --short=7 HEAD)
 ZIPNAME="Cryo-ginkgo-$(date '+%Y%m%d')-$head.zip"
-TC_DIR="$HOME/android/toolchains/aosp-clang"
-GCC_64_DIR="$HOME/android/toolchains/llvm-arm64"
-GCC_32_DIR="$HOME/android/toolchains/llvm-arm"
-AK3_DIR="$HOME/android/AnyKernel3"
+TC_DIR="$HOME/Android/Cryo/toolchains/aosp-clang"
+GCC_64_DIR="$HOME/Android/Cryo/toolchains/llvm-arm64"
+GCC_32_DIR="$HOME/Android/Cryo/toolchains/llvm-arm"
+AK3_DIR="$HOME/Android/Cryo/AnyKernel3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
-export KBUILD_BUILD_USER=adithya
-export KBUILD_BUILD_HOST=ghostrider_reborn
-
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
-fi
+export KBUILD_BUILD_USER=lyahi
+export KBUILD_BUILD_HOST=idut
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-  make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-  cp out/defconfig arch/arm64/configs/$DEFCONFIG
-  exit
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-  rm -rf out
+	rm -rf out
 fi
 
 mkdir -p out
@@ -36,26 +31,18 @@ echo -e "\nStarting compilation...\n"
 make -j$(($(nproc) + 1)) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-  echo -e "\nKernel compiled succesfully! Zipping up...\n"
-  if [ -d "$AK3_DIR" ]; then
-    cp -r $AK3_DIR AnyKernel3
-  elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3; then
-    echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-    exit 1
-  fi
-  cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-  cp out/arch/arm64/boot/dtbo.img AnyKernel3
-  rm -f *zip
-  cd AnyKernel3
-  git checkout master &>/dev/null
-  zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-  cd ..
-  rm -rf AnyKernel3
-  rm -rf out/arch/arm64/boot
-  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-  echo "Zip: $ZIPNAME"
-  [ -x "$(command -v gdrive)" ] && gdrive upload --share "$ZIPNAME"
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp out/arch/arm64/boot/Image.gz-dtb "$AK3_DIR"
+		cp out/arch/arm64/boot/dtbo.img "$AK3_DIR"
+		cd "$AK3_DIR" || exit
+		zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+		cd "$OLDPWD" || exit
+		rm -rf out/arch/arm64/boot
+		echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+		echo "Zip: $ZIPNAME"
+	fi
 else
-  echo -e "\nCompilation failed!"
-  exit 1
+	echo -e "\nCompilation failed!"
+	exit 1
 fi
